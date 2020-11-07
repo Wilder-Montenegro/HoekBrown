e11aef7e34ffec7fe06f606447f77d39919a54f3
Wilder-Montenegro <68178258+Wilder-Montenegro@users.noreply.github.com>
2020-11-06 19:51:40 -0500

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,23 @@
+
+// CREADO CON CHART.JS
+var ctx = document.getElementById('myChart').getContext('2d');
+var chart = new Chart(ctx, {
+    // The type of chart we want to create
+    type: 'line',
+
+    // The data for our dataset
+    data: {
+        labels: ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio'],
+        datasets: [{
+            label: 'My First dataset',
+            backgroundColor: 'rgb(255, 99, 132)',
+            borderColor: 'rgb(255, 99, 132)',
+            data: [0, 10, 5, 2, 20, 30, 0]
+        }]
+    },
+
+    // Configuration options go here
+    options: {}
+});
+
+
